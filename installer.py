476a91eb4efec4570fcb12a9306541b009a4b826
Avery Brown <avery@acbcg.xyz>
2018-11-03 15:44:25 -0400

    Installer - laid out functions for installation procedure

--- installer.py
+++ installer.py
@@ -5,8 +5,8 @@ import os
 import PySide2.QtWidgets as QtW
 
 
-def is_admin(dev):
-    if dev:
+def is_admin():
+    if "dev" in sys.argv:
         return True
     else:
         try:
@@ -17,9 +17,9 @@ def is_admin(dev):
 
 
 def main():
-    if is_admin(True):
+    if is_admin():
         app = QtW.QApplication(sys.argv)
-        w = RenderFarmingInstaller()
+        w = RenderFarmingInstallerMainWindow()
         w.show()
         app.exec_()
 
@@ -33,50 +33,92 @@ def create_directory(directory):
         os.makedirs(directory)
 
 
+def get_icons():
+    return "icon1", "icon2"
+
+
+def get_scripts():
+    return "script1", "script2"
+
+
+def get_macros():
+    return list("macro1")
+
+
+def get_max_dir():
+    return "C:\\Program Files\\Autodesk\\3ds Max 2018\\"
+
+
+def get_enu_dir():
+    return os.path.realpath(os.path.join(os.getenv('LOCALAPPDATA'), 'Autodesk', '3dsMax', '2018 - 64bit', 'ENU'))
+
+
+def get_user_scripts_dir():
+    return os.path.realpath(os.path.join(get_enu_dir(), 'scripts'))
+
+
+def get_user_macros_dir():
+    return os.path.realpath(os.path.join(get_enu_dir(), 'usermacros'))
+
+
+def copy_icons(icons, folders):
+    return
+
+
+def copy_file_list(files, folder):
+    return
+
+
 def run_installation(ui):
-    # ui.progress_bar.set_tasks(10)
-    # icons = get_icons()
-    # scripts = get_scripts()
-    # macros = get_macros()
+    ui.progress_bar.set_tasks(10)
+    icons = get_icons()
+    scripts = get_scripts()
+    macros = get_macros()
 
-    # max_dir = get_max_dir()
-    # icons_light_dir = os.path.join(max_dir, "UI_ln", "Icons", "Light", "RenderFarming")
-    # icons_dark_dir = os.path.join(max_dir, "UI_ln", "Icons", "Dark", "RenderFarming")
+    max_dir = get_max_dir()
+    icons_light_dir = os.path.join(max_dir, "UI_ln", "Icons", "Light", "RenderFarming")
+    icons_dark_dir = os.path.join(max_dir, "UI_ln", "Icons", "Dark", "RenderFarming")
 
-    # max_user_scripts_dir = get_user_scripts_dir()
-    # install_dir = os.path.join(max_user_scripts_dir, "BDF", "renderFarming")
+    max_user_scripts_dir = get_user_scripts_dir()
+    install_dir = os.path.join(max_user_scripts_dir, "BDF", "renderFarming")
 
-    # max_user_macros_dir = get_user_macros_dir()
+    max_user_macros_dir = get_user_macros_dir()
 
-    # ui.progress_bar.add()
+    ui.progress_bar.add()
 
-    # try:
-    #     create_directory(icons_light_dir)
+    try:
+        create_directory(icons_light_dir)
+        ui.progress_bar.add()
+        create_directory(icons_dark_dir)
+        ui.progress_bar.add()
+        create_directory(install_dir)
+        ui.progress_bar.add()
+    except (IOError, os.error) as e:
+        ui.error_msg(e)
+        return
+
+    try:
+        copy_icons(icons, icons_light_dir)
+        copy_file_list(scripts, install_dir)
+        copy_file_list(macros, max_user_macros_dir)
         # ui.progress_bar.add()
-    #     create_directory(icons_dark_dir)
-        # ui.progress_bar.add()
-    #     create_directory(install_dir)
-        # ui.progress_bar.add()
-    # except (IOError, os.error) as e:
-    #     ui.error_msg(e)
-    #     return
-
-    # try:
-    #     copy_icons(icons, icons_light_dir, icons_dark_dir)
-    #     copy_scripts(scripts, install_dir)
-    #     copy_scripts(macros, max_user_macros_dir)
-    #     # ui.progress_bar.add()
-    # except (IOError, os.error) as e:
-    #     ui.error_msg(e)
-    #     return
+    except (IOError, os.error) as e:
+        ui.error_msg(e)
+        return
 
     print("Bada Bing")
 
 
-class RenderFarmingInstaller(QtW.QDialog):
+class RenderFarmingInstaller:
+
+    def __init__(self, ui):
+        self._max_version = "2018"
+
+
+class RenderFarmingInstallerMainWindow(QtW.QDialog):
 
     def __init__(self, parent=None):
-        super(RenderFarmingInstaller, self).__init__(parent)
+        super(RenderFarmingInstallerMainWindow, self).__init__(parent)
 
         self._main_layout = QtW.QVBoxLayout()
         self._intro_page = InstallerIntroPage()
